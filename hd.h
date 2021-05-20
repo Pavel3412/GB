72e93feaaa397a49ec01efd71895b2114fceece5
Павел Добров <paveldobrov@MacBook-Pro-Pavel.local>
2021-05-20 17:26:28 +0300

    hw3

---
+++ hd.h
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int a = 6;
+int b = 1;
+int c = 4;
+int d = 3;
+double res;
--- main.cpp
+++ main.cpp
@@ -1,22 +1,13 @@
 #include<iostream>
-
+#include"hd.h"
 int main(){
-    int a = 1;
-    float f = 1.432;
-    double d = 1.4535;
-    char ch = 'A';
-    bool b = true;
-    enum variants {X, O};
-    variants arr[2] =  {X, O};
-    char game_field[3][3] = {{'X', ' ', ' '},
-    {'O','X',' '},
-    {'O', 'O', 'X'}};
-    std::cout << game_field[2][2];
-    struct game_board{
-    int game_number;
-    int game_turn;
-    char correct_game_board[3] [3];
-    };
-    
-    return 0;
+    res = a * (b + (c / d));
+    printf("%.2lf\n", res);
+    res = 0;
+    int f;
+    std::cin >> f;
+    res = (f > 21) ? (f-21) * 2 : (f-21);
+    std::cout << res;
+    int arr[3][3][3];
+    std::cout << *(*(*(arr + 1) + 1) + 1) << "\n";
 }
