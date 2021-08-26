481676234f633101f71dbda748405c6ebd0f8215
Pavel3412 <pavel.dobrov.01@gmail.com>
2021-08-26 22:50:57 +0300

    HWork2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+int prime_numb(int a) {
+    std::cin >> a;
+    int k = 1;
+    if (a > 1) {
+        for(int i = 2; i < a; ++i){
+            if(a % i == 0) { 
+                k = 0;
+                break;
+            }
+        }
+    }
+    else std::cout << "Incorrect input";
+    if(k == 1 || a > 1) std::cout << "Prime number";
+    else std::cout << "Not prime number";
+    
+}
+
+
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    int a;
+    prime_numb(a);
 }
